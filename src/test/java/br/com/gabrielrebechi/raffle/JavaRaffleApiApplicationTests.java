65abbde947ae66d3867738a2b021012037a9e07a
Gabriel Rebechi <luiz.rebechi@doc360.com.br>
2025-12-13 14:31:35 -0300

    chore: initial project setup with Spring Boot

---
+++ src/test/java/br/com/gabrielrebechi/raffle/JavaRaffleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielrebechi.raffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRaffleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
